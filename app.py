a8025e5e2e58f9b183412bd60a383876879e1ade
BurgosNY <pedroburgos@gmail.com>
2024-05-17 19:28:24 -0300

    Mais exemplos

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from datetime import datetime
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/hora_certa')
+def hora_certa():
+    import time
+    time.sleep(5)
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return current_time
 
 
 if __name__ == '__main__':
